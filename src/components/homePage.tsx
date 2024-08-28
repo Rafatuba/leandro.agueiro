5cb536550b73fffb1344cde7aa952fc012018933
Rafatuba <rafaolly1@gmail.com>
2024-08-28 12:29:22 -0400

    first commit

---
+++ src/components/homePage.tsx
@@ -0,0 +1,233 @@
+import imagemPessoas from "../../public/pessoas.jpg";
+import Image from "next/image";
+
+export function HomePage() {
+  return (
+    <div className="bg-gray-50">
+      <header className="py-4 md:py-6">
+        <div className="container px-4 mx-auto sm:px-6 lg:px-8">
+          <div className="flex items-center justify-start gap-3 ml-28">
+            <div className="flex-shrink-0">
+              <a
+                href="#"
+                title=""
+                className="flex rounded outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+              >
+                <img
+                  className="w-auto h-8"
+                  src="https://d33wubrfki0l68.cloudfront.net/682a555ec15382f2c6e7457ca1ef48d8dbb179ac/f8cd3/images/logo.svg"
+                  alt=""
+                />
+              </a>
+            </div>
+
+            {/* <div className="flex lg:hidden">
+              <button type="button" className="text-gray-900">
+                <svg
+                  className="w-7 h-7"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </button>
+            </div> */}
+
+            <div className="hidden lg:flex lg:ml-10 xl:ml-16 lg:items-center lg:justify-center lg:space-x-8 xl:space-x-16">
+              <a
+                href="#"
+                title=""
+                className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+              >
+                {" "}
+                Serviços{" "}
+              </a>
+
+              <a
+                href="#"
+                title=""
+                className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+              >
+                {" "}
+                Industries{" "}
+              </a>
+
+              <a
+                href="#"
+                title=""
+                className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+              >
+                {" "}
+                Fees{" "}
+              </a>
+
+              <a
+                href="#"
+                title=""
+                className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+              >
+                {" "}
+                About Rareblocks{" "}
+              </a>
+            </div>
+
+            {/* <div className="hidden lg:ml-auto lg:flex lg:items-center lg:space-x-8 xl:space-x-10">
+              <a
+                href="#"
+                title=""
+                className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+              >
+                {" "}
+                Sign in{" "}
+              </a>
+
+              <a
+                href="#"
+                title=""
+                className="px-5 py-2 text-base font-bold leading-7 text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl hover:bg-gray-600 font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                role="button"
+              >
+                Create free account
+              </a>
+            </div> */}
+          </div>
+        </div>
+      </header>
+
+      <section className="pt-12 pb-12 sm:pb-16 lg:pt-8">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="grid max-w-lg grid-cols-1 mx-auto lg:max-w-full lg:items-center lg:grid-cols-2 gap-y-12 lg:gap-x-32">
+            <div>
+              <div className="text-center lg:text-left">
+                <h1 className="text-4xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:leading-tight lg:text-6xl font-pj">
+                  Mantenha as finanças do seu negócio sob controle.
+                </h1>
+                <p className="mt-2 text-lg text-gray-600 sm:mt-8 font-inter">
+                  Se está faltando tempo para{" "}
+                  <strong>gerenciar seu negócio</strong>, contrate já um{" "}
+                  <strong>ASSISTENTE FINANCEIRO VIRTUAL</strong> e deixe sua
+                  vida melhor.
+                </p>
+
+                <form
+                  action="https://wa.me/5567984043097?text=Tenho%20interesse%20em%20comprar%20seu%20carro"
+                  // method="POST"
+                  className="mt-8 sm:mt-10"
+                >
+                  {/* <div className="relative p-2 sm:border sm:border-gray-400 group sm:rounded-xl sm:focus-within:ring-1 sm:focus-within:ring-gray-900 sm:focus-within:border-gray-900">
+                    <input
+                      type="email"
+                      name=""
+                      id=""
+                      placeholder="Enter email address"
+                      className="block w-full px-4 py-4 text-gray-900 placeholder-gray-900 bg-transparent border border-gray-400 outline-none focus:border-gray-900 focus:ring-1 focus:ring-gray-900 rounded-xl sm:border-none sm:focus:ring-0 sm:focus:border-transparent"
+                      // required=""
+                    />
+                  </div> */}
+                  <div className="mt-4 sm:mt-0  sm:inset-y-0 sm:right-0 sm:flex sm:items-center sm:pr-2">
+                    <a href="https://www.whatsapp.com">
+                      <button
+                        // type="submit"
+                        className="inline-flex px-6 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 rounded-lg focus:outline-none focus:bg-gray-600 font-pj hover:bg-gray-600"
+                      >
+                        Falar por Whatsapp
+                      </button>
+                    </a>
+                  </div>
+                </form>
+              </div>
+
+              <div className="flex items-center justify-center mt-10 space-x-6 lg:justify-start sm:space-x-8">
+                <div className="flex items-center">
+                  <p className="text-3xl font-medium text-gray-900 sm:text-4xl font-pj">
+                    2943
+                  </p>
+                  <p className="ml-3 text-sm text-gray-900 font-pj">
+                    Empresas
+                    <br />
+                    Atendidas
+                  </p>
+                </div>
+
+                <div className="hidden sm:block">
+                  <svg
+                    className="text-gray-400"
+                    width="16"
+                    height="39"
+                    viewBox="0 0 16 39"
+                    fill="none"
+                    stroke="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <line
+                      x1="0.72265"
+                      y1="10.584"
+                      x2="15.7226"
+                      y2="0.583975"
+                    ></line>
+                    <line
+                      x1="0.72265"
+                      y1="17.584"
+                      x2="15.7226"
+                      y2="7.58398"
+                    ></line>
+                    <line
+                      x1="0.72265"
+                      y1="24.584"
+                      x2="15.7226"
+                      y2="14.584"
+                    ></line>
+                    <line
+                      x1="0.72265"
+                      y1="31.584"
+                      x2="15.7226"
+                      y2="21.584"
+                    ></line>
+                    <line
+                      x1="0.72265"
+                      y1="38.584"
+                      x2="15.7226"
+                      y2="28.584"
+                    ></line>
+                  </svg>
+                </div>
+
+                <div className="flex items-center">
+                  <p className="text-3xl font-medium text-gray-900 sm:text-4xl font-pj">
+                    $1M+
+                  </p>
+                  <p className="ml-3 text-sm text-gray-900 font-pj">
+                    Transações
+                    <br />
+                    financeiras
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-center items-center">
+              <Image
+                className="w-[25rem] rounded-md"
+                src={imagemPessoas}
+                alt=""
+              />
+              <a
+                className="text-xs"
+                href="https://br.freepik.com/fotos-gratis/pessoas-que-participam-do-evento-de-negocios_24483100.htm#fromView=search&page=3&position=3&uuid=38a0d7f3-10a3-4599-a652-43a05d6ea88f"
+              >
+                Imagem de freepik
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
