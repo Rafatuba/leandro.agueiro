a4b3dc2e71995c12af4125c251e13d5236ba9c2d
Rafatuba <rafaolly1@gmail.com>
2024-09-05 14:47:23 -0400

    criação da sessão Quem Somos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { HomePage } from "@/components/homePage";
+import { QuemSomos } from "@/components/quemSomos";
 import { Servicos } from "@/components/servicos";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <HomePage />
       <Servicos />
+      <QuemSomos />
     </>
   );
 }
